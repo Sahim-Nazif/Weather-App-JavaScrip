e6e1d9cf15a2c443ef20d8085140931d665f1c30
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-04-22 19:07:23 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let searchInp=document.querySelector('.weather_search')
+let city= document.querySelector('.weather_city');
+let day= document.querySelector('.weather_day')
+let humidity=document.querySelector('.weather_indicator-humidity>.value')
+let wind=document.querySelector('.weather_indicator-wind>.value')
+let pressure=document.querySelector('.weather_indicator-pressure>.value')
+let image=document.querySelector('.weather_image')
+let temprature=document.querySelector('.weather_temprature')
+
+
+let weatherAPIKey='d4c3a8aefba09b7cf93c189487b1d070';
+let weatherBaseEndPoint='https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + weatherAPIKey;
+
+
+let getWeatherByCityName=async (city)=>{
+
+    let endPoint=weatherBaseEndPoint+'&q='+ city;
+
+    let response=await fetch(endPoint)
+    let weather=await response.json();
+
+     return weather;
+  
+
+}
+searchInp.addEventListener('keydown', async(e)=>{
+
+    if (e.keyCode===13){
+        let weather=await getWeatherByCityName(searchInp.value)
+
+        console.log(weather)
+    }
+})
+
+
